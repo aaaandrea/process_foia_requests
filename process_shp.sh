70993273f33db92359662b640aff7e641101089e
Andrea del Rio <adelrio@gmail.com>
2019-02-20 13:37:46 -0800

    add license

--- process_shp.sh
+++ process_shp.sh
@@ -16,7 +16,7 @@ FILEPATH=$2
 
 if [[ -z $OUTPUT ]]; then
     echo "Usage:"
-    echo "  ./process_sh <title-of-file-output> 'zipfile'"
+    echo '  ./process_sh <title-of-file-output> "zipfile"'
     echo ""
     echo "Example"
     echo "./process_sh us-ca-city_of_new_york nyc.zip"
@@ -25,7 +25,25 @@ fi
 
 
 echo "Unzipping data file"
-unzip $FILEPATH
+# mkdir unzipped
+cd unzipped
+# unzip "../${FILEPATH}"
+# cd ../
+OIFS=$IFS
+IFS='.'
+for FILE in * # cycles through all files in directory (case-sensitive!)
+do
+    echo "converting file: $FILE..."
+    echo "> [$FILE]"
+    # FILENEW=`echo $FILE | sed "s/.mif/_new.shp/"` # replaces old filename
+    # ogr2ogr \
+    # -f "ESRI Shapefile" \
+    # "$FILENEW" "$FILE"
+done
+
+# rm -rf unzipped
+
+IFS=$OIFS
 
 echo
 w
