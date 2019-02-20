2b553b3ac1d2b81f5cbfd0b2e688f0f373120817
Andrea del Rio <adelrio@gmail.com>
2019-02-19 17:52:23 -0800

    unzip file

--- process_shp.sh
+++ process_shp.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+set -e -o pipefail
+
 # open zip
 # go over each file in zip
 # convert each file
 # zip all files
-echo "Please provide the following arguments with your script: "
-echo "--output"
-echo "the output is the name of the file you wish to output"
-echo "filename"
-echo "the filename is the zip file of the source data you downloaded"
+
+# us-ca-city_of_<X>.zip
+#   - us-ca-city_of_<X>.shp
+#   - us-ca-city_of_<X>.dbf
+#   - ...
+
+OUTPUT=$1
+FILEPATH=$2
+
+if [[ -z $OUTPUT ]]; then
+    echo "Usage:"
+    echo "  ./process_sh <title-of-file-output> 'zipfile'"
+    echo ""
+    echo "Example"
+    echo "./process_sh us-ca-city_of_new_york nyc.zip"
+    exit
+fi
+
+
+echo "Unzipping data file"
+unzip $FILEPATH
+
+echo
+w
+echo
 
 # for FILE in *.zip # cycles through all files in directory (case-sensitive!)
 # do
@@ -17,4 +39,18 @@ echo "the filename is the zip file of the source data you downloaded"
 #     -f "ESRI Shapefile" \
 #     "$FILENEW" "$FILE"
 # done
+
+# VERB=$1
+
+#
+# case $VERB in
+#     download)   download;;
+#     convert)    convert;;
+#     split)    split;;
+#     transform)  parallel "transform {}" ::: ${regions[@]};;
+#     upload)     parallel "upload {}" ::: ${regions[@]};;
+#     *)          echo "not ok - invalid command" && exit 3;;
+# esac
+
+echo "Finishing up"
 exit 0
