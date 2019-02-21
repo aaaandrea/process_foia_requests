979b184199c0bead435a012afeced9e550212e7b
Andrea del Rio <adelrio@gmail.com>
2019-02-21 09:40:25 -0800

    add conditional and cleanup

--- process_shp.sh
+++ process_shp.sh
@@ -15,60 +15,54 @@ OUTPUT=$1
 FILEPATH=$2
 
 if [[ -z $OUTPUT ]]; then
+    echo "Missing name of output"
     echo "Usage:"
-    echo '  ./process_sh <title-of-file-output> "zipfile"'
+    echo '  ./process_sh <country-region-layer> "path/to/zipfile"'
     echo ""
     echo "Example"
-    echo "./process_sh us-ca-city_of_new_york nyc.zip"
+    echo './process_sh us-ca-city_of_new_york "../Downloads/nyc.zip"'
+    exit
+fi
+
+if [[ -z $FILEPATH ]]; then
+    echo "Missing filepath"
+    echo "Usage:"
+    echo '  ./process_sh <country-region-layer> "path/to/zipfile"'
+    echo ""
+    echo "Example"
+    echo './process_sh us-ca-city_of_new_york "../Downloads/nyc.zip"'
     exit
 fi
 
 
-echo "Unzipping data file"
+echo "Unzipping data..."
 # mkdir unzipped
+
 cd unzipped
 # unzip "../${FILEPATH}"
-# cd ../
+
+
 OIFS=$IFS
 IFS='.'
+
+echo "Processing data files..."
 for FILE in * # cycles through all files in directory (case-sensitive!)
 do
     echo "converting file: $FILE..."
-    echo "> [$FILE]"
+    NEWFILE= echo $OUTPUT
+    echo $NEWFILE
     # FILENEW=`echo $FILE | sed "s/.mif/_new.shp/"` # replaces old filename
     # ogr2ogr \
     # -f "ESRI Shapefile" \
     # "$FILENEW" "$FILE"
 done
 
-# rm -rf unzipped
-
-IFS=$OIFS
-
-echo
-w
-echo
-
-# for FILE in *.zip # cycles through all files in directory (case-sensitive!)
-# do
-#     echo "converting file: $FILE..."
-#     FILENEW=`echo $FILE | sed "s/.mif/_new.shp/"` # replaces old filename
-#     ogr2ogr \
-#     -f "ESRI Shapefile" \
-#     "$FILENEW" "$FILE"
-# done
-
-# VERB=$1
-
-#
-# case $VERB in
-#     download)   download;;
-#     convert)    convert;;
-#     split)    split;;
-#     transform)  parallel "transform {}" ::: ${regions[@]};;
-#     upload)     parallel "upload {}" ::: ${regions[@]};;
-#     *)          echo "not ok - invalid command" && exit 3;;
-# esac
 
 echo "Finishing up"
+cd ../
+# rm -rf unzipped/
+IFS=$OIFS
+
+
+
 exit 0
